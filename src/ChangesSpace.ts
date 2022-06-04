346bb8aaa14b6aa9017511cd3c437ae9b948bb86
Denis Voronchikhin <adaptmen@gmail.com>
2022-06-05 03:01:18 +0500

    Add validators for ChangesSpace

--- src/ChangesSpace.ts
+++ src/ChangesSpace.ts
@@ -3,12 +3,18 @@ import { deepCopy } from "./deepCopy";
 import { deepEqual } from "./deepEqual";
 
 
-type ChangeRecord = {
+export type ChangeRecord = {
   progress: <T>(val: T, meta: any) => T;
   meta: any;
 };
 
 type StackIdent = string;
+
+export type ChangesValidator<T = any> = (progressed: T) => boolean;
+
+function defaultValidator(progressed) {
+  return true;
+}
 
 export class ChangesSpace {
 
@@ -16,11 +22,17 @@ export class ChangesSpace {
 
   private initialMap = new Map<StackIdent, any>();
 
+  private validatorsMap = new Map<StackIdent, ChangesValidator>();
+
   getStack(ident: StackIdent): ChangesStack<ChangeRecord> {
     if (!this.stackMap.has(ident)) {
       this.stackMap.set(ident, new ChangesStack());
     }
     return this.stackMap.get(ident);
+  }
+
+  setValidator(ident: StackIdent, validator: ChangesValidator) {
+    this.validatorsMap.set(ident, validator);
   }
 
   updateInitial(ident: StackIdent, initial: any) {
@@ -47,6 +59,16 @@ export class ChangesSpace {
     const initialEqualMapped = stacksIdents.map(ident => deepEqual(this.progressed[ident], this.initialMap.get(ident)));
     const someNotEqualInitial = initialEqualMapped.some(val => !val);
     return someNotEqualInitial;
+  }
+
+  get validated(): {[ident: StackIdent]: boolean} {
+    return Object.keys(this.stackMap).reduce((acc, ident) => {
+      const progressed = this.progressed[ident];
+      const validator = this.validatorsMap.get(ident) || defaultValidator;
+      const valid = validator(progressed);
+      acc[ident] = valid;
+      return acc;
+    }, {});
   }
 
 }
